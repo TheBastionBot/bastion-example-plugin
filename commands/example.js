29a0512e110a41397aa16a234e44c3f044b37fb8
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-05-20 16:53:50 +0530

    commands: add example command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/example.js
@@ -0,0 +1,39 @@
+/*!
+ * @plugin bastion-example-plugin
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @license MIT
+ */
+
+"use strict";
+
+const tesseract = require("tesseract");
+
+class ExampleCommandPlugin extends tesseract.Command {
+    constructor() {
+        super("example", {
+            description: "This is an example command. It logs the Command object, the message that triggered the command, and the argumens passed to this command.",
+            triggers: [ "ex", "eg" ],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            typing: false,
+            schedulable: true,
+            unsafe: false,
+            nsfw: false,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [],
+            syntax: [],
+            condition: () => true,
+        });
+    }
+
+    exec = async (message, argv) => {
+        console.log("COMMAND: ", this);
+        console.log("MESSAGE: ", message);
+        console.log("ARGUMENTS: ", argv);
+    }
+}
+
+module.exports = ExampleCommandPlugin;
